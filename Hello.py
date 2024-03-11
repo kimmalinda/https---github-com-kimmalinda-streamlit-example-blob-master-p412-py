a8f55bf886e33c8901f457a571aa1d1fdd59fcc8
kimmalinda <161007836+kimmalinda@users.noreply.github.com>
2024-03-11 12:23:14 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,13 +4,7 @@ import joblib
 import numpy as np
 import pandas as pd
 #Model
-from sklearn.metrics import classification_report, accuracy_score, make_scorer, confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import accuracy_score
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
+
 
 st.title('การทำนายระดับผลการเรียน')
 st.markdown('กรุณากรอกข้อมูลให้ครบเพื่อใช้ในการทำนาย')
